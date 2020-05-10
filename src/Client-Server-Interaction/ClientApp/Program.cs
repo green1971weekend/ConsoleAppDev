42c3f6ace80dd63303a207c6c2bf6e878fb4d5c0
Straltsou Maxim <green1971weekend1@gmail.com>
2020-05-10 21:46:56 +0300

    feat: added the simple client-server simulator

---
+++ src/Client-Server-Interaction/ClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ClientServer.BL;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new ClientServerController();
+
+            Console.WriteLine("Вас приветствует приложение Client\n");
+
+            controller.EstablishClientConnection();
+        }
+
+    }
+}
---
+++ src/Client-Server-Interaction/Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using ClientServer.BL;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new ClientServerController();
+
+            Console.WriteLine("Запуск сервера\n");
+            Console.WriteLine("Ожидание входящих подключений...\n");
+
+            controller.EstablishServerConnection();
+
+        }
+    }
+}
